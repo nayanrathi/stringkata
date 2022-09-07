e7ff872bbc3c698ccf95a33562448cd9245d61c8
Nayan Rathi <56042373+nayanrathi@users.noreply.github.com>
2022-09-07 23:45:30 +0530

    Create code.java

---
+++ code.java
@@ -0,0 +1,8 @@
+public class StringCalculator
+    {
+        public int AddNumbers(string args)
+        {
+            if (string.IsNullOrEmpty(args))
+            {
+                return 0;
+            }
